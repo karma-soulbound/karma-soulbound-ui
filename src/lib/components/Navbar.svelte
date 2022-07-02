7c48832f31fdda59c2085d68b1651694b09acf7f
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-07-02 15:35:08 +0700

    add history href to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,7 +5,7 @@
   <div class="flex-none">
     <ul class="menu menu-horizontal p-0">
       <li><a href='/search'>สำรวจกรรม</a></li>
-      <li><a href='/'>สมุดบรรทึกกรรม</a></li>
+      <li><a href='/history'>สมุดบรรทึกกรรม</a></li>
       <li><a href='/create'>สร้างกรรม</a></li>
     </ul>
   </div>
