5da3ea49026184ca233aa6e012024a2f9b3156aa
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-07-02 13:46:25 +0700

    fix: replace svelte.head with sveltekit.head

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,11 +4,16 @@
 
 <center>
   <div class='top-0 absolute left-0 right-0 mx-auto'>
-    <div class='max-w-min bg-black/30 flex flex-row items-center justify-center gap-2 rounded-full p-1 m-3'>
+    <div class='max-w-min bg-black/30 flex flex-row items-center justify-center gap-2 rounded-[100px] p-1 px-2 m-3'>
       <h3 class='{$page.url.pathname === "/" ? "nav-active" : "nav-item"}'>Karma</h3>
       <h3 class='{$page.url.pathname === "/p1" ? "nav-active" : "nav-item"}'>Bruh</h3>
       <h3 class='{$page.url.pathname === "/p2" ? "nav-active" : "nav-item"}'>Lmao</h3>
       <h3 class='{$page.url.pathname === "/p3" ? "nav-active" : "nav-item"}'>Smth</h3>
+    </div>
+  </div>
+  <div class='top-0 absolute right-0 m-3'>
+    <div class='flex'>
+      <button>Connect นรก</button>
     </div>
   </div>
 </center>
