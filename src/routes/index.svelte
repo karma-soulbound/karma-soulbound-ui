1eebe56fb67b4283979676d472273703de3a4737
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-07-02 12:27:21 +0700

    feat: creating home page

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<main class='min-h-screen bg-gradient-to-b from-[#30003d] to-[#403f40]'>
+  <div class='flex flex-col min-h-screen w-full items-center justify-center'>
+    <div class='flex flex-col gap-4 items-center justify-center bg-black/25 p-8 rounded-xl shadow-lg'>
+      <h1 class='md:text-5xl font-bold text-white'>
+        กรรมใดใครก่อ
+      </h1>
+      <div class='flex flex-col gap-2 w-full'>
+        <input type='text' placeholder='Input 1'>
+        <input type='text' placeholder='Input 2'>
+      </div>
+      <button>
+        กรรม... อะไรนะลืม
+      </button>
+    </div>
+  </div>
+</main>
