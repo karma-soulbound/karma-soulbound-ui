094cdcb209b07cfa8ab16a925920d8d6319e995a
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-07-02 20:50:09 +0700

    feat: add ahosigum buttons

--- src/lib/components/KarmaCard.svelte
+++ src/lib/components/KarmaCard.svelte
@@ -7,35 +7,30 @@
   export let pic;
 </script>
 
-<a
-  href="#"
-  class="block w-full bg-grey rounded-lg border shadow-md md:flex-row md:max-w-xl bg-white dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 min-w-[75%] mb-4 hover:text-black opacity-80"
->
-  <div class="flex">
-    <div class="card-body">
-    
-   
-    <figure><img src={pic} alt="Shoes" /></figure>
-    <div class="flex-1">
-    <div class="flex flex-col justify-between p-4 leading-normal">
-    <h2 class="card-title">{title}</h2>
-      <p>{description}</p>
-      <progress class="progress progress-error w-full" value={scale} max="100"></progress>
-      {scale}% {stepName[Math.round(scale / 10) * 10]}
+<a href='#'>
+  <div class='card w-96 h-64 bg-base-100 shadow-xl image-full shadow-lg'>
+    <div class='card-body flex flex-col justify-between'>
+      <div class='flex flex-col gap-1'>
+        <h2 class='card-title'>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div class='flex flex-col gap-1'>
+        <progress class='progress progress-error w-full' max='100' value={scale}></progress>
+        {scale}% {stepName[Math.round(scale / 10) * 10]}
+        <div class='flex flex-row gap-2'>
+          <button class='btn btn-success'>
+            อโหสิกรรม
+          </button>
+          <button class='btn btn-error'>
+            กรรมตามสนอง
+          </button>
+        </div>
+      </div>
     </div>
-    </div>
-    <div class="p-4">
-    <div class="mb-1 text-base font-medium text-red-700">Karma Scale : {scale}</div>
-    <div class="w-full bg-gray-200 rounded-full h-2.5">
-      <div class="bg-red-600 h-2.5 rounded-full" style={`width: ${scale}%`} />
-    </div>
-    <div class="flex justify-start gap-3 items-center pt-4">
-        <button class="p-2 rounded-md bg-red-500 text-black hover:bg-red-800 hover:text-white">รับกรรมทันที</button>
-        <button class="p-2 rounded-md bg-green-500 text-black hover:bg-green-800 hover:text-white">ขออโหสิกรรม</button>
-    </div>
-</div>
-</div>
+    <figure><img alt='img' src='{pic}' /></figure>
+  </div>
 </a>
+
 <!-- <a>
   <div class="card w-96 bg-base-100 shadow-xl image-full">
    
